28d7ec575eed6c0542825e85d757972fbc0d12c5
DELL <DELL@DELL-PC>
2019-06-29 11:12:02 +0530

    ABCDE

--- B18CucuRestAssured/src/test/java/com/basic/Testinggoogle.java
+++ B18CucuRestAssured/src/test/java/com/basic/Testinggoogle.java
@@ -11,19 +11,18 @@ import io.restassured.response.Response;
 
 public class Testinggoogle {
 	
-/*	public static void main (String args[]){
+	public static void main (String args[]){
 	Response res = RestAssured.given().get("https://www.google.com/");
 	int statCode = res.statusCode();
 	System.out.println("Status code="+statCode);
 	}
-		*/
-	
-	@Test
-	public void testGoogle(){
-		Response res = RestAssured.given().get("https://www.google.com/");
-		int statCode = res.statusCode();
-		System.out.println("Status code="+statCode);
-		Assert.assertEquals(statCode, 200);
-		}	
+		
+//	@Test
+//	public void testGoogle(){
+//		Response res = RestAssured.given().get("https://www.google.com/");
+//		int statCode = res.statusCode();
+//		System.out.println("Status code="+statCode);
+//		Assert.assertEquals(statCode, 200);
+//		}	
 
 }
