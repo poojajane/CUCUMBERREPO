091fc65f496799106e4c085bf06ff606af88dd37
DELL <DELL@DELL-PC>
2019-06-29 11:15:09 +0530

    VAGFHAGJ

--- B18CucuRestAssured/src/test/java/com/basic/Testinggoogle.java
+++ B18CucuRestAssured/src/test/java/com/basic/Testinggoogle.java
@@ -3,7 +3,7 @@ package com.basic;
 
 
 import org.junit.Assert;
-import org.testng.annotations.Test;
+//import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
